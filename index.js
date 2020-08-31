d403a3bc942cdeb54e3723cf875f88de0b64d9a2
kids <kidanov_anton@mail.ru>
2020-08-31 18:12:53 +0400

    Lesson 7. Registered routes(homeRoutes, coursesRoutes, addRoutes). Modified index.js(it is cleaned and understanded)

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses');
+const addRoutes = require('./routes/add');
+
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
@@ -9,30 +13,13 @@ const hbs = exphbs.create({
 
 app.use(express.static('public'));
 
+app.use('/', homeRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/add', addRoutes);
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Главная страница',
-    isHome: true,
-  });
-});
-
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Список курсов',
-    isCourses: true,
-  });
-});
-
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Добавить новый курс',
-    isAdd: true,
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 
