820c1d8a809a6a6fbe5eb36daa2cfc554c7c0172
kids <kidanov_anton@mail.ru>
2020-09-10 15:06:20 +0400

    Add mongodb connect

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const app = express();
 
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 
 const Course = require('./models/course');
 
@@ -13,12 +16,20 @@ const hbs = exphbs.create({
   extname: 'hbs',
 });
 
-app.use(express.static('public'));
+hbs._renderTemplate = function (template, context, options) {
+  options.allowProtoMethodsByDefault = true;
+  options.allowProtoPropertiesByDefault = true;
+
+  return template(context, options);
+};
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/add', addRoutes);
+app.use('/card', cardRoutes);
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -26,6 +37,20 @@ app.set('views', 'views');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+async function start() {
+  try {
+    const url = `mongodb+srv://kids_:BbVXi-H6mwfAfm7@cluster0.xmdhu.mongodb.net/shop`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
--- models/course.js
+++ models/course.js
@@ -1,66 +1,103 @@
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-const path = require('path');
-const { resolve } = require('path');
+const { Schema, model } = require('mongoose');
 
-class Course {
-  constructor(title, price, img) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuidv4();
-  }
+const course = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  img: String,
+});
 
-  toJSON() {
-    return {
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id,
-    };
-  }
-  async save() {
-    const courses = await Course.getAll();
+module.exports = model('Course', course);
 
-    courses.push(this.toJSON());
+// const { v4: uuidv4 } = require('uuid');
+// const fs = require('fs');
+// const path = require('path');
+// const { resolve } = require('path');
 
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        JSON.stringify(courses),
-        (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        }
-      );
-    });
+// class Course {
+//   constructor(title, price, img) {
+//     this.title = title;
+//     this.price = price;
+//     this.img = img;
+//     this.id = uuidv4();
+//   }
 
-    console.log('Courses:', courses);
-  }
+//   static async update(course) {
+//     const courses = await Course.getAll();
 
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        path.join(__dirname, '..', 'data', 'courses.json'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(content));
-          }
-        }
-      );
-    });
-  }
+//     const idx = courses.findIndex((c) => c.id === course.id);
+//     courses[idx] = course;
 
-  static async getById(id) {
-    const courses = await Course.getAll();
-    return courses.find((c) => c.id === id);
-  }
-}
+//     return new Promise((resolve, reject) => {
+//       fs.writeFile(
+//         path.join(__dirname, '..', 'data', 'courses.json'),
+//         JSON.stringify(courses),
+//         (err) => {
+//           if (err) {
+//             reject(err);
+//           } else {
+//             resolve();
+//           }
+//         }
+//       );
+//     });
+//   }
 
-module.exports = Course;
+//   toJSON() {
+//     return {
+//       title: this.title,
+//       price: this.price,
+//       img: this.img,
+//       id: this.id,
+//     };
+//   }
+//   async save() {
+//     const courses = await Course.getAll();
+
+//     courses.push(this.toJSON());
+
+//     return new Promise((resolve, reject) => {
+//       fs.writeFile(
+//         path.join(__dirname, '..', 'data', 'courses.json'),
+//         JSON.stringify(courses),
+//         (err) => {
+//           if (err) {
+//             reject(err);
+//           } else {
+//             resolve();
+//           }
+//         }
+//       );
+//     });
+
+//     console.log('Courses:', courses);
+//   }
+
+//   static getAll() {
+//     return new Promise((resolve, reject) => {
+//       fs.readFile(
+//         path.join(__dirname, '..', 'data', 'courses.json'),
+//         'utf-8',
+//         (err, content) => {
+//           if (err) {
+//             reject(err);
+//           } else {
+//             resolve(JSON.parse(content));
+//           }
+//         }
+//       );
+//     });
+//   }
+
+//   static async getById(id) {
+//     const courses = await Course.getAll();
+//     return courses.find((c) => c.id === id);
+//   }
+// }
+
+// module.exports = Course;
